6e23cac29ed1ece7c9c324ab2cc9d0ae7ad7536f
giselecoder <gisele.tavares@yuzer.com.br>
2025-03-08 16:37:27 -0300

    Exemplos do Cap 5

---
+++ src/Cap5/Usuario.java
@@ -0,0 +1,18 @@
+package Cap5;
+
+public class Usuario {
+    private String nome;
+
+    public Usuario(String nome) {
+        this.nome = nome;
+    }
+
+    // getters e setters
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
